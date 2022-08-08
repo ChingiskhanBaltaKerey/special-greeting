eead4a0c22dec6beb14ef7ef314c815a4f90f6c9
Chingiskhan Nugrazin <110384143+ChingiskhanBaltaKerey@users.noreply.github.com>
2022-08-08 14:38:27 +0600

    greeting Alla

---
+++ dotnet_special_greeting/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Alla")
+{
+     Console.WriteLine("Hi Honey! I love U");
+}
+else 
+{
+    Console.Write ("Salamaleikum, ");
+    Console.WriteLine (username);
+}
