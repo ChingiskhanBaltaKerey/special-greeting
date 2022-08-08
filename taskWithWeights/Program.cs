fac2b9a86863afe81abacfe356bd13854599ac2e
Chingiskhan Nugrazin <110384143+ChingiskhanBaltaKerey@users.noreply.github.com>
2022-08-08 14:52:00 +0600

    Find max weight

---
+++ taskWithWeights/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 15;
+int b = 33;
+int c = 12;
+int d = 3;
+int e = 99;
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("max= ");
+Console.WriteLine(max);
